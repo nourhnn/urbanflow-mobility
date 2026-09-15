b810e9c491f6ae073b8d8990ccbedc22b611ff2f
nourhnn <148661667+nourhnn@users.noreply.github.com>
2026-09-15 18:52:26 +0200

    Update page.tsx

--- app/inscription/page.tsx
+++ app/inscription/page.tsx
@@ -135,7 +135,7 @@ export default function InscriptionPage() {
               id="firstName"
               name="firstName"
               label="Prénom"
-              placeholder="Nour"
+              placeholder="Prénom"
               autoComplete="given-name"
               required
             />
